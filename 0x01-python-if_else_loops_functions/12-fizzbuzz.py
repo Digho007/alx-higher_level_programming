03203a90b1ca94ebb3f5ebdc93cfa2acaec0f68e
Digho007 <dighomanorjeremiah@gmail.com>
2023-01-04 09:29:56 +0000

    task

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,11 @@
 
 def fizzbuzz():
     for i in range (1, 101):
-        print (i, end="")
         if i % 3 == 0:
             print ("Fizz", end="")
         if i % 5 == 0:
             print ("Buzz", end="")
-        elif i % 15 == 0:
+        if i % 15 == 0:
             print ("FizzBuzz", end="")
+        else:
+            print (i, end="")
