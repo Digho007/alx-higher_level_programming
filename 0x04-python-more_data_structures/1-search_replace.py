a21c5f29c83fb74a4d6ae60cf22d3b32f5a38318
Digho007 <dighomanorjeremiah@gmail.com>
2023-01-13 19:19:02 +0000

    task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     def find_search(element):
         return element if element != search else replace
-    new_list = []
+    new_list = my_list
     return list(map(find_search, new_list))
