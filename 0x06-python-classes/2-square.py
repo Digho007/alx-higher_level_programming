8691fb2abb59081a758680c06865cccb259b27b1
Digho007 <dighomanorjeremiah@gmail.com>
2023-01-25 02:41:14 +0000

    task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+This module provides a simple Square class with initialized size.
+"""
+
+
 class Square:
-    """create a class named Square
-    """
-
+    """A class that defines a Square by size"""
     def __init__(self, size=0):
-        """__init__ method initializes an instance
-        Arg:
-            size: private instance attribute
-        Raises:
-            TypeError: size must be an integer
-            ValueError: size must be greater or equal than zero
-        """
-
-        self.__size = size
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
+        if type(size) != int:
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError('size must be >= 0')
+            raise ValueError("size must be >= 0")
+        self.__size = size
