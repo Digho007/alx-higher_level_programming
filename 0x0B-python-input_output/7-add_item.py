8a32ae979b4dfc697e4d5edb774b6c3b86314c44
Digho007 <dighomanorjeremiah@gmail.com>
2023-02-13 12:04:06 +0000

    task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+import sys
 
-from sys import argv
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+def save_to_json_file(my_obj, filename):
+    import json
+    with open(filename, mode='w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
 
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
 
-for arg in argv[1:]:
-    json_list.append(arg)
+def load_from_json_file(filename):
+    import json
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
 
-save_to_json_file(json_list, filename)
+
+if __name__ == "__main__":
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
